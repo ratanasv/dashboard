6e4a597b6ca510c6f9f028c2f6926fea4dbfc42f
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-05-03 12:57:48 -0700

    yay successfully use angular's service API.

--- draft/app/areaChart/areaChart.js
+++ draft/app/areaChart/areaChart.js
@@ -31,45 +31,48 @@ angular.module('cs519Assign3.areaChart', [
 
 	myAreaChart = areaChart(config);
 	myAreaChart.height(100);
-	myAreaChart();
+	myAreaChart.render();
 }])
 
 .factory('areaChart', ['reusableChart', function(reusableChart) {
-	var my = {};
-	reusableChart(my);
-	var chart = d3.select('#chart')
-		.attr('width', my.width)
-		.attr('height', my.height);
-
-	var area;
-
-	var dataYMax;
-	var dataYMin;
-	var y;
-	
-
 	return function(config) {
-		dataYMax = Math.max.apply(null, data);
-		dataYMin = Math.min.apply(null, data);
+		var dataYMax;
+		var dataYMin;
+
+		var my = {};
+		reusableChart(my);
+
+		dataYMax = Math.max.apply(null, config.data);
+		dataYMin = Math.min.apply(null, config.data);
 
 		if (dataYMin > 0) {
 			dataYMin = 0;
 		}
 
-		y = d3.scale.linear().domain([dataYMin, dataYMax]).range([my.height, 0]);
-		area = d3.svg.area()
-			.x(function(d, i) {
-				return i*my.width/config.data.length;
-			})
-			.y0(my.height)
-			.y1(function(d, i) {
-				return y(d);
-			});
+		my.render = function() {;
+			var chart = d3.select('#chart')
+				.attr('width', my.width())
+				.attr('height', my.height());
 
-		chart.append('path')
-			.datum(config.data)
-			.attr('class', 'area')
-			.attr('d', area);
+			var area;
+			var y;
+			
+			y = d3.scale.linear().domain([dataYMin, dataYMax]).range([my.height(), 0]);
+			area = d3.svg.area()
+				.x(function(d, i) {
+					return i*my.width()/config.data.length;
+				})
+				.y0(my.height())
+				.y1(function(d, i) {
+					return y(d);
+				});
+
+			chart.append('path')
+				.datum(config.data)
+				.attr('class', 'area')
+				.attr('d', area);
+		}
+		
 		return my;
 	};
 }]);
