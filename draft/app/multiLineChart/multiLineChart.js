2eccde0a52efccd66b270b91c744bef136fb429a
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-05-03 18:17:47 -0700

    finish multiline chart.

--- draft/app/multiLineChart/multiLineChart.js
+++ draft/app/multiLineChart/multiLineChart.js
@@ -1,5 +1,3 @@
-var data = [4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17, 4, 8, 15, 16, 8, 42, 40, 2, 10, 17];
-
 angular.module('cs519Assign3.multiLineChart', [
 	'cs519Assign3.util'
 ])
@@ -16,37 +14,85 @@ angular.module('cs519Assign3.multiLineChart', [
 	})
 })
 
-.controller('MultiLineChartCtrl', ['$scope', 'multiLineChart', function($scope, multiLineChart) {
-	var config;
-	var myMultiLineChart;
+.controller('MultiLineChartCtrl', ['$scope', 'multiLineChart', 'randomArray', function($scope, multiLineChart, randomArray) {
+		var config;
+		var myMultiLineChart;
 
-	$scope.heightSlider = {
-		name: 'Height Slider',
-		value: '200'
-	}
-	$scope.widthSlider = {
-		name: 'Width Slider',
-		value: '800'
-	}
+		$scope.heightSlider = {
+			name: 'Height Slider',
+			value: '200'
+		}
+		$scope.widthSlider = {
+			name: 'Width Slider',
+			value: '800'
+		}
 
-	$scope.height = 200;
-	$scope.width = 800;
+		config = {
+			data: [randomArray(100), randomArray(200), randomArray(300)],
+			width: $scope.widthSlider.value,
+			height: $scope.heightSlider.value
+		};
 
-	config = {
-		data: data,
-		width: $scope.width,
-		height: $scope.height
-	};
-
-	/*
-	myMultiLineChart = multiLineChart(config);
-	myMultiLineChart.height(100);
-	myMultiLineChart.render()
-	*/
+		
+		myMultiLineChart = multiLineChart(config);
+		myMultiLineChart.render()
 }])
 
+
 .factory('multiLineChart', ['reusableChart', function(reusableChart) {
-	return {
-		yomama: 'isfat'
+	return function(config) {
+		var dataYMax;
+		var dataYMin;
+
+		var my = {};
+		var color = d3.scale.category10();
+		reusableChart(my);
+
+		if (config.width) {
+			my.width(config.width);
+		}
+		if (config.height) {
+			my.height(config.height);
+		}
+
+		dataYMax = Math.max.apply(null, config.data[0]);
+		dataYMin = Math.min.apply(null, config.data[0]);
+
+		if (dataYMin > 0) {
+			dataYMin = 0;
+		}
+
+		my.render = function() {
+			var chart = d3.select('#chart')
+				.attr('width', my.width())
+				.attr('height', my.height());
+
+			var line;
+			var y;
+			
+			y = d3.scale.linear().domain([dataYMin, dataYMax]).range([my.height(), 0]);
+			line = d3.svg.line()
+				.interpolate('basis')
+				.x(function(d, i) {
+					return i*my.width()/config.data[0].length;
+				})
+				.y(function(d, i) {
+					return y(d);
+				});
+
+			d3.select('#multiLineChartPath').remove();
+
+			chart.selectAll('path')
+				.data(config.data)
+				.enter()
+				.append('path')
+				.attr('class', 'line')
+				.attr('d', line)
+				.style('stroke', color(1))
+				.style('fill', 'none');
+
+		};
+		
+		return my;
 	};
 }]);
