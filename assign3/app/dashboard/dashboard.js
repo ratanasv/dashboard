2ce62d68de8d8c7f5069bd0addbad58c5d0f38ac
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-05-21 12:23:13 -0700

    add dashboard page with simple squares that dynamically resize.

--- assign3/app/dashboard/dashboard.js
+++ assign3/app/dashboard/dashboard.js
@@ -1,5 +1,6 @@
 angular.module('cs519Assign3.dashboard', [
-	'templates-app'
+	'templates-app',
+	'cs519Assign3.util'
 ])
 
 .config(function($stateProvider) {
@@ -14,6 +15,40 @@ angular.module('cs519Assign3.dashboard', [
 	});
 })
 
-.controller('DashboardCtrl', function() {
-	
-});
+.controller('DashboardCtrl', ['$scope', 'sliderInitHelper', function($scope, sliderInitHelper) {
+	sliderInitHelper($scope, {
+		widthSlider: {
+			name: 'Width Slider',
+			minValue: 0,
+			maxValue: 1200,
+			value: 800
+		},
+		heightSlider: {
+			name: 'Height Slider',
+			minValue: 0,
+			maxValue: 1200,
+			value: 300
+		},
+		sizeSlider: {
+			name: 'Size Slider',
+			minValue: 0,
+			maxValue: 100,
+			value: 50
+		}
+	});
+
+	$scope.servers = [];
+	for (var i=0; i<10; i++) {
+		$scope.servers.push({
+			id: i
+		});
+	}
+
+	$scope.serverPositionX = function(id) {
+		return id*1.2*$scope.sizeSlider.value;
+	};
+
+	$scope.serverPositionY = function() {
+		return 10;
+	};
+}]);
