e081f1636efa2028549bceb715bf87fff53e6ad1
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2014-05-23 18:06:12 -0700

    socket.io dependencies for server/client.

--- assign3/build_config.js
+++ assign3/build_config.js
@@ -4,7 +4,8 @@ module.exports = {
 		'bower_components/angular/angular.js',
 		'bower_components/angular-ui-router/release/angular-ui-router.js',
 		'bower_components/underscore/underscore.js',
-		'bower_components/d3/d3.js'
+		'bower_components/d3/d3.js',
+		'bower_components/socket.io-client/dist/socket.io.js'
 	],
 	source_js: [
 		'app/**/*.js', '!app/**/*.spec.js'
